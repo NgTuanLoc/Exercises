2fb13a2a4b5e48c1af731f5b90fcc6cd2ab3fb1e
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-09-24 18:33:34 +0700

    menu section

--- coffee_shop/src/data/data.js
+++ coffee_shop/src/data/data.js
@@ -3,6 +3,13 @@ import cartItem2 from "../images/cart-item-2.png";
 import cartItem3 from "../images/cart-item-3.png";
 import cartItem4 from "../images/cart-item-4.png";
 
+import menuItem1 from "../images/menu-1.png";
+import menuItem2 from "../images/menu-2.png";
+import menuItem3 from "../images/menu-3.png";
+import menuItem4 from "../images/menu-4.png";
+import menuItem5 from "../images/menu-5.png";
+import menuItem6 from "../images/menu-6.png";
+
 export const cartItems = [
   {
     img: cartItem1,
@@ -23,5 +30,44 @@ export const cartItems = [
     img: cartItem4,
     name: "Item 4",
     price: "10$",
+  },
+];
+
+export const menuItems = [
+  {
+    img: menuItem1,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
+  },
+  {
+    img: menuItem2,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
+  },
+  {
+    img: menuItem3,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
+  },
+  {
+    img: menuItem4,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
+  },
+  {
+    img: menuItem5,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
+  },
+  {
+    img: menuItem6,
+    name: "Tasty and Healthy",
+    beforePrice: "20.99",
+    finalPrice: "15.99",
   },
 ];
--- coffee_shop/src/sections/Menu.js
+++ coffee_shop/src/sections/Menu.js
@@ -1,11 +1,26 @@
+import { menuItems } from "../data/data";
+
 const Menu = () => {
   return (
     <section className="menu" id="menu">
       <h1 className="heading">
-          <span>Our</span> Menu
+        <span>Our</span> Menu
       </h1>
       <div className="menu__container">
-          <div className="menu__item"></div>
+        {menuItems.map((item, id) => {
+          return (
+            <div className="menu__item" key={id}>
+              <img src={item.img} alt="menu-item" />
+              <h3>{item.name}</h3>
+              <div className="price">
+                ${item.finalPrice} <span>${item.beforePrice}</span>
+              </div>
+              <a href="#home" className="btn">
+                add to cart
+              </a>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
